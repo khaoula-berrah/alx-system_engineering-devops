ced82cd87fb3bbe6066e8ca72fd456b521fc992c
khaoula-berrah <berah.khaoula@gmail.com>
2023-08-01 13:57:00 -0700

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+log_file = ARGV[0]
+
+File.open(log_file, 'r') do |file|
+  file.each_line do |line|
+    match = line.match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
+    if match
+      sender = match[1]
+      receiver = match[2]
+      flags = match[3]
+      puts "#{sender},#{receiver},#{flags}"
+    end
+  end
+end
