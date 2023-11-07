22300e475fdca0cd918b1a70b641ddc09c2decd8
khaoula-berrah <berah.khaoula@gmail.com>
2023-11-07 06:32:58 -0800

    0x16-api_advancced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,72 +1,34 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
-import re
-import requests
-import sys
-
-
-def add_title(dictionary, hot_posts):
-    """ Adds item into a list """
-    if len(hot_posts) == 0:
-        return
-
-    title = hot_posts[0]['data']['title'].split()
-    for word in title:
-        for key in dictionary.keys():
-            c = re.compile("^{}$".format(key), re.I)
-            if c.findall(word):
-                dictionary[key] += 1
-    hot_posts.pop(0)
-    add_title(dictionary, hot_posts)
-
-
-def recurse(subreddit, dictionary, after=None):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': u_agent
-    }
-
-    params = {
-        'after': after
-    }
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-
-    if res.status_code != 200:
-        return None
-
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    add_title(dictionary, hot_posts)
-    after = dic['data']['after']
-    if not after:
-        return
-    recurse(subreddit, dictionary, after=after)
+""" model parses the title of all hot articles """
 
 
 def count_words(subreddit, word_list):
-    """ Init function """
-    dictionary = {}
+    """
+    query the Reddit API, parse the title of all hot articles,
+    and print a sorted count of given keywords
+    """
+    import requests
 
-    for word in word_list:
-        dictionary[word] = 0
+    url = f"https://reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
 
-    recurse(subreddit, dictionary)
-
-    l = sorted(dictionary.items(), key=lambda kv: kv[1])
-    l.reverse()
-
-    if len(l) != 0:
-        for item in l:
-            if item[1] is not 0:
-                print("{}: {}".format(item[0], item[1]))
-    else:
+    if response.status_code != 200:
         print("")
+        return
+
+    data = response.json()
+    count = {}
+    for post in data.get("data", {}).get("children", []):
+        title = post.get("data", {}).get("title", "").lower()
+        for word in word_list:
+            word = word.lower()
+            if word in title:
+                count[word] = count.get(word, 0) + 1
+
+    for key, value in sorted(count.items(), key=lambda x: x[1]):
+        print("{}: {}".format(key, value))
+
+
+count_words("programming",
+            "react python java javascript scala no_results_for_this_one".split())
